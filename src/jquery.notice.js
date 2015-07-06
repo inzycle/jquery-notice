8fe449b2cbf69d4c790c1afaf954c02a86e9f57a
Sigma <sm.salvador@gmail.com>
2015-07-06 11:31:43 -0500

    Updating Functionality - Hide/Destroy on empty containers

--- src/jquery.notice.js
+++ src/jquery.notice.js
@@ -18,7 +18,12 @@
         CLASS_STICK = 'jq-notice-stick',
         CLASS_SCROLL = 'jq-notice-scroll',
         CLASS_CONTENT = 'jq-notice-content',
-        CLASS_LIST = 'jq-notice-list';
+        CLASS_LIST = 'jq-notice-list',
+        CLASS_ITEM_WRAP = 'jq-notice-item-wrap',
+        CLASS_ITEM = 'jq-notice-item',
+        CLASS_ITEM_CONTENT = 'jq-notice-item-content',
+        CLASS_ITEM_CLOSE = 'jq-notice-item-close',
+        CLASS_INACTIVE = 'jq-inactive';
 
     var defaults = {
             align: 'bottom right',
@@ -31,16 +36,16 @@
         },
         settings = {},
         $div = $('<div>').addClass(CLASS_DIV).append(
-            $('<div>').addClass(CLASS_STICK).addClass(CLASS_SCROLL).append(
+            $('<div>').addClass(CLASS_STICK).addClass(CLASS_SCROLL).addClass(CLASS_INACTIVE).append(
                 $('<div>').addClass(CLASS_CONTENT)
             )
         ).append(
-            $('<div>').addClass(CLASS_LIST).addClass(CLASS_SCROLL).append(
+            $('<div>').addClass(CLASS_LIST).addClass(CLASS_SCROLL).addClass(CLASS_INACTIVE).append(
                 $('<div>').addClass(CLASS_CONTENT)
             )
         ),
-        div_stick = '.jq-notice-stick .jq-notice-content',
-        div_list = '.jq-notice-list .jq-notice-content',
+        div_stick = '.' + CLASS_STICK + ' .' + CLASS_CONTENT,
+        div_list = '.' + CLASS_LIST + ' .' + CLASS_CONTENT,
         _notice_events = [],
         _notice_action = false;
 
@@ -51,7 +56,7 @@
         setAlign();
         setShape();
 
-        if ( ! $('body .jq-notice').length ){
+        if ( ! $(CLASS_DIV).length ){
             $('body').append($div);
         }
 
@@ -59,8 +64,10 @@
 
         if ( settings.stick ){
             $(div_stick).append(_notice);
+            $('.' + CLASS_STICK).removeClass(CLASS_INACTIVE);
         } else {
             $(div_list).append(_notice);
+            $('.' + CLASS_LIST).removeClass(CLASS_INACTIVE);
         }
 
         if ( ! _notice_action ){ _notice_next(); }
@@ -107,27 +114,28 @@
 
         function _notice_factory(text, delay, speed, stick, priority, callback) {
 
-            var obj = $('<div>').addClass('jq-notice-item-wrap')
-                .append($('<div>').addClass('jq-notice-item').addClass('priority-' + priority)
-                    .append($('<span>').addClass('jq-notice-item-content').text(text))
-                    .append($('<span>').addClass('jq-notice-item-close'))
+            var obj = $('<div>').addClass(CLASS_ITEM_WRAP)
+                .append($('<div>').addClass(CLASS_ITEM).addClass('priority-' + priority)
+                    .append($('<span>').addClass(CLASS_ITEM_CONTENT).text(text))
+                    .append($('<span>').addClass(CLASS_ITEM_CLOSE))
             );
 
             if ( stick ){
 
-                var obj_close = obj.children().children('.jq-notice-item-close');
+                var obj_close = obj.children().children('.' + CLASS_ITEM_CLOSE);
 
-                obj_close.on('click.jq-notice-item-close', function(e) {
+                obj_close.on('click.' + CLASS_ITEM_CLOSE, function(e) {
 
                     e.preventDefault();
 
-                    var _notice = $(this).parents('.jq-notice-item-wrap');
+                    var _notice = $(this).parents('.' + CLASS_ITEM_WRAP);
 
                     _notice.animate({                   // Manual Remove, ToDO: Need more options
                         opacity: 0
                     }, speed, function(){
                         obj.remove();                   // Step 1: remove html element
                         callback();                     // Step 2: execute possible callback
+                        _notice_destroy();              // Step 3: execute destroyer
                     });
 
                 });
@@ -144,6 +152,7 @@
                         callback();                     // Step 3: execute possible callback
                         _notice_action = false;         // Step 4: update global flag ( executioner )
                         _notice_next();                 // Step 5: execute next animation ( sequencer )
+                        _notice_destroy();              // Step 6: execute destroyer
                     });
                 };
 
@@ -162,6 +171,27 @@
             } else {
                 _notice_action = false;
             }
+        }
+
+        function _notice_destroy(){
+
+            var _list = false,
+                _stick = false;
+
+            if ( ! $( '.' + CLASS_STICK).find( '.' + CLASS_ITEM).length ) {
+                $('.' + CLASS_STICK).addClass(CLASS_INACTIVE);
+                _stick = true;
+            }
+
+            if ( ! _notice_events.length ) {
+                $('.' + CLASS_LIST).addClass(CLASS_INACTIVE);
+                _list = true;
+            }
+
+            if ( _stick && _list ) {
+                $div.remove();
+            }
+
         }
 
     };
